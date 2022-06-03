fe0c7cd12b3ec0e58635ed5412552da0fbcb255f
Anna Vdovina <annavdovina7068@gmail.com>
2022-06-03 10:38:44 +0400

    repo

---
+++ lab7/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,87 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab7/lab7.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			AbstractFactory* F = new NYFactory();
+			PostCard* m;
+			m = F->createPostCard();
+			Assert::IsTrue(m->give() == "NYCard::give()");
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			AbstractFactory* F = new NYFactory();
+			Present* b;
+			b = F->createPresent();
+			Assert::IsTrue(b->give() == "NYPresent::give()");
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			AbstractFactory* F = new NYFactory();
+			Cake* d;
+			d = F->createCake();
+			Assert::IsTrue(d->give()== "NYCake::give()");
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			AbstractFactory* F = new BDFactory();
+			PostCard* m;
+			m = F->createPostCard();
+			Assert::IsTrue(m->give() == "BDCard::give()");
+		}
+
+		TEST_METHOD(TestMethod5)
+		{
+			AbstractFactory* F = new BDFactory();
+			Present* b;
+			b = F->createPresent();
+			Assert::IsTrue(b->give() == "BDPresent::give()");
+		}
+
+
+		TEST_METHOD(TestMethod6)
+		{
+			AbstractFactory* F = new BDFactory();
+			Cake* d;
+			d = F->createCake();
+			Assert::IsTrue(d->give() == "BDCake::give()");
+		}
+
+		TEST_METHOD(TestMethod7)
+		{
+			AbstractFactory* F = new  MarchFactory();
+			PostCard* m;
+			m = F->createPostCard();
+			Assert::IsTrue(m->give() == "MarchCard::give()");
+		}
+
+		TEST_METHOD(TestMethod8)
+		{
+			AbstractFactory* F = new MarchFactory();
+			Present* b;
+			b = F->createPresent();
+			Assert::IsTrue(b->give() == "MarchPresent::give()");
+		}
+
+		TEST_METHOD(TestMethod9)
+		{
+			AbstractFactory* F = new MarchFactory();
+			Cake* d;
+			d = F->createCake();
+			Assert::IsTrue(d->give() == "MarchCake::give()");
+		}
+
+	};
+}
---
+++ lab7/lab7/lab7.cpp
@@ -0,0 +1,187 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// Абстрактные продукты
+struct PostCard {
+    virtual string give() = 0; //ЧВФ
+};
+
+struct Present {
+    virtual string give() = 0;//ЧВФ
+};
+
+struct Cake {
+    virtual string give() = 0;//ЧВФ
+};
+
+
+//Конкретные продукты
+struct  NYCard : public PostCard {
+    virtual string give() {
+        string res = "NYCard::give()";
+        return res;
+    }
+};
+
+struct BDCard : public PostCard {
+    virtual string give() {
+        string res = "BDCard::give()";
+        return res;
+    }
+};
+
+struct MarchCard : public PostCard {
+    virtual string give() {
+        string res = "MarchCard::give()";
+        return res;
+    }
+};
+
+struct  NYPresent : public Present {
+    virtual string give() {
+        string res ="NYPresent::give()";
+        return res;
+    }
+};
+
+struct BDPresent : public Present {
+    virtual string give() {
+        string res ="BDPresent::give()";
+        return res;
+    }
+};
+
+struct MarchPresent : public Present {
+    virtual string give() {
+        string res ="MarchPresent::give()";
+        return res;
+    }
+};
+
+struct  NYCake : public Cake {
+    virtual string give() {
+        string res ="NYCake::give()";
+        return res;
+    }
+};
+
+struct BDCake : public Cake {
+    virtual string give() {
+        string res ="BDCake::give()";
+        return res;
+    }
+};
+struct MarchCake : public Cake {
+    virtual string give() {
+        string res ="MarchCake::give()";
+        return res;
+    }
+};
+
+
+
+//Если хотим еще один уровень сложности, то нам нужна еще конкретная фабрика
+
+
+//Абстрактная фабрика
+struct AbstractFactory {
+    virtual PostCard* createPostCard() = 0; // интерфейс для создания семейств объектов
+    virtual Present* createPresent() = 0;
+    virtual Cake* createCake() = 0;//новая ЧВФ для создания нового продукта
+};
+
+//Конкретные фабрики
+struct NYFactory : public AbstractFactory {
+    virtual PostCard* createPostCard() {
+        return new NYCard();
+    }
+    virtual Present* createPresent() {
+        return new NYPresent();
+    }
+    virtual Cake* createCake() { //реализация нового метода для создания нового продукта
+        return new NYCake();
+    }
+};
+
+
+struct BDFactory : public AbstractFactory {
+    virtual PostCard* createPostCard() {
+        return new BDCard();
+    }
+    virtual Present* createPresent() {
+        return new BDPresent();
+    }
+    virtual Cake* createCake() { //реализация нового метода для создания нового продукта
+        return new BDCake();
+    }
+};
+
+struct MarchFactory : public AbstractFactory {
+    virtual PostCard* createPostCard() {
+        return new MarchCard();
+    }
+    virtual Present* createPresent() {
+        return new MarchPresent();
+    }
+    virtual Cake* createCake() {
+        return new MarchCake();
+    }
+};
+
+
+//программа-клиент
+int main() {
+
+    AbstractFactory* F = new NYFactory(); //предполагаем сначала низкий уровень сложности -> семейство "тупых" продуктов
+    PostCard* m;
+    Present* b;
+    Cake* d; //указатель на новый продукт
+
+    m = F->createPostCard();
+    b = F->createPresent();
+    d = F->createCake();
+
+    m->give();
+    b->give();
+    d->give();
+
+    delete F;
+    delete m; delete b; delete d;
+
+    cout << "------------------------" << endl;
+
+    F = new BDFactory();
+
+    m = F->createPostCard();
+    b = F->createPresent();
+    d = F->createCake();
+
+    m->give();
+    b->give();
+    d->give();
+
+    delete F;
+    delete m; delete b; delete d;
+
+    cout << "------------------------" << endl;
+
+    
+    F = new MarchFactory();
+
+    m = F->createPostCard();
+    b = F->createPresent();
+    d = F->createCake();
+
+    m->give();
+    b->give();
+    d->give();
+
+    delete F;
+    delete m; delete b; delete d;
+    
+    cout << "------------------------" << endl;
+
+
+    return 0;
+}
